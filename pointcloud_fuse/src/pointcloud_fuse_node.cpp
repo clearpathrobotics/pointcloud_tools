9ee716e246b496c3ef49f270b58dcf714327c20f
Dave Niewinski <dniewinski@clearpathrobotics.com>
2017-11-30 15:48:43 -0500

    Roslinting and minor efficiency improvements

--- pointcloud_fuse/src/pointcloud_fuse_node.cpp
+++ pointcloud_fuse/src/pointcloud_fuse_node.cpp
@@ -1,3 +1,4 @@
+
 /**
 Software License Agreement (proprietary)
 
@@ -14,7 +15,7 @@ express permission of Clearpath Robotics.
 #include <math.h>
 #include <boost/bind.hpp>
 #include <pcl_conversions/pcl_conversions.h>
-
+#include <vector>
 #include <ros/ros.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/common/transforms.h>
@@ -25,124 +26,123 @@ express permission of Clearpath Robotics.
 
 namespace pointcloud_fuse
 {
-  class PointCloudFuse
-  {
-  private:
-    float angle_step_;
-    double update_frequency_;
-    ros::NodeHandle nh_;
-    ros::Publisher point_cloud_pub_;
-    ros::Timer update_point_cloud_timer_;
-    std::string cloud_frame_id_;
-    std::string itos(int i);
-    std::vector<std::string> cloud_topics_;
-    std::vector<ros::Subscriber> cloud_subs_;
-    std::map<std::string, sensor_msgs::PointCloud2> cloud_ranges_;
-    tf::TransformListener tf_listener_;
-    void subscribeClouds();
-  public:
-    explicit PointCloudFuse(ros::NodeHandle&);
-    void cloudCallback(const ros::MessageEvent<sensor_msgs::PointCloud2>& event, const std::string& topic);
-    void updatePointCloud(const ros::TimerEvent&);
-  };
-  PointCloudFuse::PointCloudFuse(ros::NodeHandle& nh): nh_(nh)
-  {
-    // control how densely we create points in the point cloud
-    angle_step_ = 0.10f;
-    // Coordinate frame into which we transform all cloud data and publish the point cloud
-    cloud_frame_id_ = "cloud_fuse_frame";
-    // Fixed rate at which we publish a new point cloud
-    nh_.getParam("update_frequency", update_frequency_);
+class PointCloudFuse
+{
+private:
+  float angle_step_;
+  double update_frequency_;
+  ros::NodeHandle nh_;
+  ros::Publisher point_cloud_pub_;
+  ros::Timer update_point_cloud_timer_;
+  std::string cloud_frame_id_;
+  std::string itos(int i);
+  std::vector<std::string> cloud_topics_;
+  std::vector<ros::Subscriber> cloud_subs_;
+  std::map< std::string, boost::shared_ptr<sensor_msgs::PointCloud2> > clouds_;
+  tf::TransformListener tf_listener_;
+  void subscribeClouds();
+public:
+  explicit PointCloudFuse(ros::NodeHandle&);
+  void cloudCallback(const ros::MessageEvent<sensor_msgs::PointCloud2>& event, const std::string& topic);
+  void updatePointCloud(const ros::TimerEvent&);
+};
+PointCloudFuse::PointCloudFuse(ros::NodeHandle& nh): nh_(nh)
+{
+  // control how densely we create points in the point cloud
+  angle_step_ = 0.10f;
+  // Coordinate frame into which we transform all cloud data and publish the point cloud
+  cloud_frame_id_ = "cloud_fuse_frame";
+  // Fixed rate at which we publish a new point cloud
+  nh_.getParam("update_frequency", update_frequency_);
 
-    point_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("cloud", 1, true);
-    update_point_cloud_timer_ = nh_.createTimer(
-      ros::Duration(1/update_frequency_), &PointCloudFuse::updatePointCloud, this);
-    subscribeClouds();
-  }
-  void PointCloudFuse::subscribeClouds()
+  point_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("cloud", 1, true);
+  update_point_cloud_timer_ = nh_.createTimer(
+    ros::Duration(1/update_frequency_), &PointCloudFuse::updatePointCloud, this);
+  subscribeClouds();
+}
+void PointCloudFuse::subscribeClouds()
+{
+  // loop through each cloudX_topic parampcl_conversions::toPCL
+  const std::string base_name = "cloud";
+  int i = 0;
+  while (true)
   {
-    // loop through each cloudX_topic parampcl_conversions::toPCL
-    const std::string base_name = "cloud";
-    int i = 0;
-    while(true)
+    std::string current_cloud_topic_param = base_name + itos(i) + "_topic";
+    if (!nh_.hasParam(current_cloud_topic_param))
     {
-      std::string current_cloud_topic_param = base_name + itos(i) + "_topic";
-      if(!nh_.hasParam(current_cloud_topic_param))
-      {
-        ROS_DEBUG("No param %s found", current_cloud_topic_param.c_str());
-        break;
-      }
-      std::string current_cloud_topic;
-      nh_.getParam(current_cloud_topic_param, current_cloud_topic);
-      ROS_DEBUG("Adding topic %s", current_cloud_topic.c_str());
-      cloud_topics_.push_back(current_cloud_topic);
-      ++i;
+      ROS_DEBUG("No param %s found", current_cloud_topic_param.c_str());
+      break;
     }
-    // TODO complain bitterly if we do not have at least one cloud
-    // subscribe to each cloud topic we have been given
-    for (unsigned int i = 0; i < cloud_topics_.size(); ++i)
+    std::string current_cloud_topic;
+    nh_.getParam(current_cloud_topic_param, current_cloud_topic);
+    ROS_DEBUG("Adding topic %s", current_cloud_topic.c_str());
+    cloud_topics_.push_back(current_cloud_topic);
+    ++i;
+  }
+
+  for (unsigned int i = 0; i < cloud_topics_.size(); ++i)
+  {
+    ROS_INFO("Subscribing to %s", cloud_topics_[i].c_str());
+    // the boost::bind allows us to sneak in the topic name so we can tell different clouds apart inside one callback
+    cloud_subs_.push_back(
+      nh_.subscribe<sensor_msgs::PointCloud2>(
+        cloud_topics_[i], 1, boost::bind(&PointCloudFuse::cloudCallback, this, _1, cloud_topics_[i])));
+  }
+}
+void PointCloudFuse::cloudCallback(const ros::MessageEvent<sensor_msgs::PointCloud2>& event, const std::string& topic)
+{
+  clouds_[topic] = event.getMessage();
+
+  ROS_DEBUG("Retrieved value from cloud at topic %s", topic.c_str());
+}
+void PointCloudFuse::updatePointCloud(const ros::TimerEvent& te)
+{
+  sensor_msgs::PointCloud2 point_cloud_pub_msg;
+  pcl::PointCloud<pcl::PointXYZ> point_cloud_pcl;
+  point_cloud_pub_msg.header.frame_id = cloud_frame_id_;
+  point_cloud_pub_msg.header.stamp = ros::Time::now();
+
+  for (
+    std::map< std::string, boost::shared_ptr<sensor_msgs::PointCloud2> >::iterator iter = clouds_.begin();
+    iter != clouds_.end();
+    ++iter)
+  {
+    std:: string topic = iter->first;
+    boost::shared_ptr<sensor_msgs::PointCloud2> cloud_msg = iter->second;
+
+    ROS_DEBUG("Updating point cloud from topic %s with frame %s", topic.c_str(), cloud_msg->header.frame_id.c_str());
+
+    try
     {
-      ROS_INFO("Subscribing to %s", cloud_topics_[i].c_str());
-      // the boost::bind allows us to sneak in the topic name so we can tell different clouds apart inside one callback
-      cloud_subs_.push_back(
-        nh_.subscribe<sensor_msgs::PointCloud2>(
-          cloud_topics_[i], 1, boost::bind(&PointCloudFuse::cloudCallback, this, _1, cloud_topics_[i])));
+      tf::StampedTransform transform;
+      geometry_msgs::TransformStamped transform_msgs;
+      tf_listener_.lookupTransform(cloud_frame_id_, cloud_msg->header.frame_id, ros::Time(0), transform);
+      tf::transformStampedTFToMsg(transform, transform_msgs);
+
+      sensor_msgs::PointCloud2 transformed_cloud;
+      tf2::doTransform(*cloud_msg, transformed_cloud, transform_msgs);
+
+      pcl::PointCloud<pcl::PointXYZ> temp_pcl;
+      pcl::fromROSMsg(transformed_cloud, temp_pcl);
+
+      point_cloud_pcl = temp_pcl + point_cloud_pcl;
     }
-  }
-  void PointCloudFuse::cloudCallback(const ros::MessageEvent<sensor_msgs::PointCloud2>& event, const std::string& topic)
-  {
-    sensor_msgs::PointCloud2 msg = *event.getMessage();
-
-    cloud_ranges_[topic] = msg;
-    ROS_DEBUG("Retrieved value from cloud at topic %s",topic.c_str());
-  }
-  void PointCloudFuse::updatePointCloud(const ros::TimerEvent& te)
-  {
-    sensor_msgs::PointCloud2 point_cloud_pub_msg;
-    pcl::PointCloud<pcl::PointXYZ> point_cloud_pcl;
-    point_cloud_pub_msg.header.frame_id = cloud_frame_id_;
-    point_cloud_pub_msg.header.stamp = ros::Time::now();
-
-    for(
-      std::map<std::string,sensor_msgs::PointCloud2>::iterator iter = cloud_ranges_.begin();
-      iter != cloud_ranges_.end();
-      ++iter)
+    catch (tf::TransformException ex)
     {
-      std:: string topic = iter->first;
-      sensor_msgs::PointCloud2 cloud_msg = iter->second;
-      ROS_DEBUG("Updating point cloud from topic %s with frame %s", topic.c_str(), cloud_msg.header.frame_id.c_str());
-
-      try
-      {
-        tf::StampedTransform transform;
-        geometry_msgs::TransformStamped transform_msgs;
-        tf_listener_.lookupTransform(cloud_frame_id_, cloud_msg.header.frame_id, ros::Time(0), transform);
-        tf::transformStampedTFToMsg (transform, transform_msgs);
-
-        sensor_msgs::PointCloud2 transformed_cloud;
-        tf2::doTransform (cloud_msg, transformed_cloud, transform_msgs);
-
-        pcl::PointCloud<pcl::PointXYZ> temp_pcl;
-        pcl::fromROSMsg(transformed_cloud, temp_pcl);
-
-        point_cloud_pcl = temp_pcl + point_cloud_pcl;
-      }
-      catch (tf::TransformException ex){
-        ROS_WARN("%s",ex.what());
-      }
+      ROS_WARN("%s", ex.what());
     }
-
-
-    pcl::toROSMsg (point_cloud_pcl, point_cloud_pub_msg);
-    point_cloud_pub_.publish(point_cloud_pub_msg);
   }
-  std::string PointCloudFuse::itos(int i) // convert int to string
-  {
-      std::stringstream s;
-      s << i;
-      return s.str();
-  }
-} // namespace pointcloud_fuse
+
+  pcl::toROSMsg(point_cloud_pcl, point_cloud_pub_msg);
+  point_cloud_pub_.publish(point_cloud_pub_msg);
+}
+std::string PointCloudFuse::itos(int i)  // convert int to string
+{
+    std::stringstream s;
+    s << i;
+    return s.str();
+}
+}  // namespace pointcloud_fuse
 
 int main(int argc, char** argv)
 {
--- sonar_pointcloud/src/sonar_pointcloud_node.cpp
+++ sonar_pointcloud/src/sonar_pointcloud_node.cpp
@@ -15,7 +15,7 @@ express permission of Clearpath Robotics.
 #include <boost/bind.hpp>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
-
+#include <vector>
 #include <ros/ros.h>
 #include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/Point.h>
@@ -27,139 +27,139 @@ typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
 namespace sonar_pointcloud
 {
-  class SonarPointCloud
-  {
-  private:
-    float angle_step_;
-    double update_frequency_;
-    ros::NodeHandle nh_;
-    ros::Publisher point_cloud_pub_;
-    ros::Timer update_point_cloud_timer_;
-    std::string cloud_frame_id_;
-    std::string itos(int i);
-    std::vector<std::string> sonar_topics_;
-    std::vector<ros::Subscriber> sonar_subs_;
-    std::map<std::string, sensor_msgs::Range> sonar_ranges_;
-    tf::TransformListener tf_listener_;
-    void insertRangeIntoPointCloud(PointCloud::Ptr point_cloud, float range, float field_of_view, std::string sensor_frame_id);
-    void subscribeSonars();
-  public:
-    explicit SonarPointCloud(ros::NodeHandle&);
-    void sonarCallback(const ros::MessageEvent<sensor_msgs::Range>& event, const std::string& topic);
-    void updatePointCloud(const ros::TimerEvent&);
-  };
-  SonarPointCloud::SonarPointCloud(ros::NodeHandle& nh): nh_(nh)
-  {
-    // control how densely we create points in the point cloud
-    angle_step_ = 0.10f;
-    // Coordinate frame into which we transform all sonar data and publish the point cloud
-    cloud_frame_id_ = "sonar_cloud";
-    // Fixed rate at which we publish a new point cloud
-    nh_.getParam("update_frequency", update_frequency_);
+class SonarPointCloud
+{
+private:
+  float angle_step_;
+  double update_frequency_;
+  ros::NodeHandle nh_;
+  ros::Publisher point_cloud_pub_;
+  ros::Timer update_point_cloud_timer_;
+  std::string cloud_frame_id_;
+  std::string itos(int i);
+  std::vector<std::string> sonar_topics_;
+  std::vector<ros::Subscriber> sonar_subs_;
+  std::map<std::string, sensor_msgs::Range> sonar_ranges_;
+  tf::TransformListener tf_listener_;
+  void insertRangeIntoPointCloud(PointCloud::Ptr point_cloud, float range, float field_of_view,
+    std::string sensor_frame_id);
+  void subscribeSonars();
+public:
+  explicit SonarPointCloud(ros::NodeHandle&);
+  void sonarCallback(const ros::MessageEvent<sensor_msgs::Range>& event, const std::string& topic);
+  void updatePointCloud(const ros::TimerEvent&);
+};
+SonarPointCloud::SonarPointCloud(ros::NodeHandle& nh): nh_(nh)
+{
+  // control how densely we create points in the point cloud
+  angle_step_ = 0.10f;
+  // Coordinate frame into which we transform all sonar data and publish the point cloud
+  cloud_frame_id_ = "sonar_cloud";
+  // Fixed rate at which we publish a new point cloud
+  nh_.getParam("update_frequency", update_frequency_);
 
-    point_cloud_pub_ = nh_.advertise<PointCloud>("points", 1, true);
-    update_point_cloud_timer_ = nh_.createTimer(
-      ros::Duration(1/update_frequency_), &SonarPointCloud::updatePointCloud, this);
-    subscribeSonars();
-  }
-  void SonarPointCloud::subscribeSonars()
+  point_cloud_pub_ = nh_.advertise<PointCloud>("points", 1, true);
+  update_point_cloud_timer_ = nh_.createTimer(
+    ros::Duration(1/update_frequency_), &SonarPointCloud::updatePointCloud, this);
+  subscribeSonars();
+}
+void SonarPointCloud::subscribeSonars()
+{
+  // loop through each sonarX_topic param
+  const std::string base_name = "sonar";
+  int i = 0;
+  while (true)
   {
-    // loop through each sonarX_topic param
-    const std::string base_name = "sonar";
-    int i = 0;
-    while(true)
+    std::string current_sonar_topic_param = base_name + itos(i) + "_topic";
+    if (!nh_.hasParam(current_sonar_topic_param))
     {
-      std::string current_sonar_topic_param = base_name + itos(i) + "_topic";
-      if(!nh_.hasParam(current_sonar_topic_param))
+      ROS_DEBUG("No param %s found", current_sonar_topic_param.c_str());
+      break;
+    }
+    std::string current_sonar_topic;
+    nh_.getParam(current_sonar_topic_param, current_sonar_topic);
+    ROS_DEBUG("Adding topic %s", current_sonar_topic.c_str());
+    sonar_topics_.push_back(current_sonar_topic);
+    ++i;
+  }
+
+  for (unsigned int i = 0; i < sonar_topics_.size(); ++i)
+  {
+    ROS_INFO("Subscribing to %s", sonar_topics_[i].c_str());
+    // the boost::bind allows us to sneak in the topic name so we can tell different sonars apart inside one callback
+    sonar_subs_.push_back(
+      nh_.subscribe<sensor_msgs::Range>(
+        sonar_topics_[i], 1, boost::bind(&SonarPointCloud::sonarCallback, this, _1, sonar_topics_[i])));
+  }
+}
+void SonarPointCloud::insertRangeIntoPointCloud(
+  PointCloud::Ptr point_cloud, float range, float field_of_view, std::string sensor_frame_id)
+{
+  for (float th = 0.0; th < field_of_view; th += angle_step_)
+  {
+    for (float ph = 0.0; ph < M_PI*2; ph += angle_step_)
+    {
+      geometry_msgs::PointStamped ps_sonarframe;
+      ps_sonarframe.header.stamp = ros::Time::now();
+      ps_sonarframe.header.frame_id = sensor_frame_id;
+      ps_sonarframe.point.z = range*sin(th)*cos(ph);
+      ps_sonarframe.point.y = range*sin(th)*sin(ph);
+      ps_sonarframe.point.x = range*cos(th);
+
+      geometry_msgs::PointStamped ps_cloudframe;
+      tf_listener_.waitForTransform(cloud_frame_id_, sensor_frame_id, ros::Time::now(), ros::Duration(3.0));
+      try
       {
-        ROS_DEBUG("No param %s found", current_sonar_topic_param.c_str());
-        break;
+        tf_listener_.transformPoint(cloud_frame_id_, ps_sonarframe, ps_cloudframe);
       }
-      std::string current_sonar_topic;
-      nh_.getParam(current_sonar_topic_param, current_sonar_topic);
-      ROS_DEBUG("Adding topic %s", current_sonar_topic.c_str());
-      sonar_topics_.push_back(current_sonar_topic);
-      ++i;
-    }
-    // TODO complain bitterly if we do not have at least one sonar
-    // subscribe to each sonar topic we have been given
-    for (unsigned int i = 0; i < sonar_topics_.size(); ++i)
-    {
-      ROS_INFO("Subscribing to %s", sonar_topics_[i].c_str());
-      // the boost::bind allows us to sneak in the topic name so we can tell different sonars apart inside one callback
-      sonar_subs_.push_back(
-        nh_.subscribe<sensor_msgs::Range>(
-          sonar_topics_[i], 1, boost::bind(&SonarPointCloud::sonarCallback, this, _1, sonar_topics_[i])));
-    }
-  }
-  void SonarPointCloud::insertRangeIntoPointCloud(
-    PointCloud::Ptr point_cloud, float range, float field_of_view, std::string sensor_frame_id)
-  {
-    for (float th = 0.0; th < field_of_view; th += angle_step_)
-    {
-      for (float ph = 0.0; ph < M_PI*2; ph += angle_step_)
+      catch(tf::TransformException ex)
       {
-        geometry_msgs::PointStamped ps_sonarframe;
-        ps_sonarframe.header.stamp = ros::Time::now();
-        ps_sonarframe.header.frame_id = sensor_frame_id;
-        ps_sonarframe.point.z = range*sin(th)*cos(ph);
-        ps_sonarframe.point.y = range*sin(th)*sin(ph);
-        ps_sonarframe.point.x = range*cos(th);
-
-        geometry_msgs::PointStamped ps_cloudframe;
-        tf_listener_.waitForTransform(cloud_frame_id_,sensor_frame_id, ros::Time::now(), ros::Duration(3.0));
-        try
-        {
-          tf_listener_.transformPoint(cloud_frame_id_, ps_sonarframe, ps_cloudframe);
-        }
-        catch(tf::TransformException ex)
-        {
-          ROS_ERROR("%s", ex.what());
-          return;
-        }
-        pcl::PointXYZ result(ps_cloudframe.point.x, ps_cloudframe.point.y, ps_cloudframe.point.z);
-        point_cloud->points.push_back(result);
+        ROS_ERROR("%s", ex.what());
+        return;
       }
+      pcl::PointXYZ result(ps_cloudframe.point.x, ps_cloudframe.point.y, ps_cloudframe.point.z);
+      point_cloud->points.push_back(result);
     }
   }
-  void SonarPointCloud::sonarCallback(const ros::MessageEvent<sensor_msgs::Range>& event, const std::string& topic)
+}
+void SonarPointCloud::sonarCallback(const ros::MessageEvent<sensor_msgs::Range>& event, const std::string& topic)
+{
+  sensor_msgs::Range msg = *event.getMessage();
+  // avoid processing invalid values
+  if ((msg.range > msg.max_range) || (msg.range < msg.min_range))
   {
-    sensor_msgs::Range msg = *event.getMessage();
-    // avoid processing invalid values
-    if ((msg.range > msg.max_range) || (msg.range < msg.min_range))
-    {
-      sensor_msgs::Range msg_empty;
-      sonar_ranges_[topic] = msg_empty;
-      return;
-    }
-    sonar_ranges_[topic] = msg;
-    ROS_DEBUG("Retrieved value from sonar at topic %s: %f",topic.c_str(),sonar_ranges_[topic].range);
+    sensor_msgs::Range msg_empty;
+    sonar_ranges_[topic] = msg_empty;
+    return;
   }
-  void SonarPointCloud::updatePointCloud(const ros::TimerEvent& te)
-  {
-    PointCloud::Ptr point_cloud(new PointCloud());
-    point_cloud->header.frame_id = cloud_frame_id_;
-    pcl_conversions::toPCL(ros::Time::now(), point_cloud->header.stamp);
+  sonar_ranges_[topic] = msg;
+  ROS_DEBUG("Retrieved value from sonar at topic %s: %f", topic.c_str(), sonar_ranges_[topic].range);
+}
+void SonarPointCloud::updatePointCloud(const ros::TimerEvent& te)
+{
+  PointCloud::Ptr point_cloud(new PointCloud());
+  point_cloud->header.frame_id = cloud_frame_id_;
+  pcl_conversions::toPCL(ros::Time::now(), point_cloud->header.stamp);
 
-    for(
-      std::map<std::string,sensor_msgs::Range>::iterator iter = sonar_ranges_.begin();
-      iter != sonar_ranges_.end();
-      ++iter)
-    {
-      std:: string topic = iter->first;
-      sensor_msgs::Range range_msg = iter->second;
-      ROS_DEBUG("Updating point cloud from topic %s with range %f", topic.c_str(), range_msg.range);
-      insertRangeIntoPointCloud(point_cloud, range_msg.range, range_msg.field_of_view, range_msg.header.frame_id);
-    }
-    point_cloud_pub_.publish(point_cloud);
-  }
-  std::string SonarPointCloud::itos(int i) // convert int to string
+  for (
+    std::map<std::string, sensor_msgs::Range>::iterator iter = sonar_ranges_.begin();
+    iter != sonar_ranges_.end();
+    ++iter)
   {
-      std::stringstream s;
-      s << i;
-      return s.str();
+    std:: string topic = iter->first;
+    sensor_msgs::Range range_msg = iter->second;
+    ROS_DEBUG("Updating point cloud from topic %s with range %f", topic.c_str(), range_msg.range);
+    insertRangeIntoPointCloud(point_cloud, range_msg.range, range_msg.field_of_view, range_msg.header.frame_id);
   }
-} // namespace sonar_pointcloud
+  point_cloud_pub_.publish(point_cloud);
+}
+std::string SonarPointCloud::itos(int i)  // convert int to string
+{
+    std::stringstream s;
+    s << i;
+    return s.str();
+}
+}  // namespace sonar_pointcloud
 
 int main(int argc, char** argv)
 {
